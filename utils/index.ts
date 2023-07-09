8b38d1960b7c92eb383b111156cd33e73ae457bf
SaiAbhinav <saiabhinavvasepalli@gmail.com>
2023-07-09 16:45:04 +0530

    Initial Commit

---
+++ utils/index.ts
@@ -0,0 +1,48 @@
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, fuel, limit } = filters;
+
+    const headers = {
+        "X-RapidAPI-Key": "d861f5c6ecmshf31d44190dc35efp145214jsn5e02ecbcdbce",
+        "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    };
+
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+        { headers: headers }
+    );
+
+    const result = await response.json();
+
+    return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50;
+    const mileageFactor = 0.1;
+    const ageFactor = 0.05;
+
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    return "/hero.png";
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    searchParams.set(type, value);
+
+    const newPathname = `${
+        window.location.pathname
+    }?${searchParams.toString()}`;
+
+    return newPathname;
+};
